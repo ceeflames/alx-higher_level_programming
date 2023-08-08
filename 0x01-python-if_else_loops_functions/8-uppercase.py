236e569f84f441c9003e83ee4bad9143232e9c78
ceeflames <ceedizprodigy@gmail.com>
2023-08-08 09:28:51 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,7 @@ def uppercase(str):
     n = ""
     for i in range(len(str)):
         if ord(str[i]) in range(97, 123):
-            n = n + chr(ord(str[i]) -32)
+            n = n + chr(ord(str[i]) - 32)
         else:
             n = n + str[i]
     print("{}".format(n))
-    
