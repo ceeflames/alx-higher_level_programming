d3583f88a2eb265bef39bdef9780c469c01dff70
ceeflames <ceedizprodigy@gmail.com>
2023-08-08 09:27:36 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    n = ""
+    for i in range(len(str)):
+        if ord(str[i]) in range(97, 123):
+            n = n + chr(ord(str[i]) -32)
+        else:
+            n = n + str[i]
+    print("{}".format(n))
+    
