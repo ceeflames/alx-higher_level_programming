d193a10b9b730ba60c91a11a9910ca38b2dfa8b1
ceeflames <ceedizprodigy@gmail.com>
2023-08-12 02:25:36 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    n = len(argv) - 1
+
+    sum = 0
+    if n == 0:
+        print(sum)
+    for i in range(n):
+        sum += int(argv[i + 1])
+    print("{}".format(sum))
