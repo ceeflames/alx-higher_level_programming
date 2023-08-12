0b7ba6d2c68535049282a99a2f12ccb34f36455f
ceeflames <ceedizprodigy@gmail.com>
2023-08-12 04:15:37 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import hidden_4
 
     name = dir(hidden_4)
-    for n in range(len(name)):
-        if name[:2] != "__":
+    for n in name:
+        if n[0] != "_":
             print(n)
