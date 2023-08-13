088ce1f92aaa777e6265ec651630bc744e1050fa
ceeflames <ceedizprodigy@gmail.com>
2023-08-13 02:00:25 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1, sys
+    import sys, calculator_1
     
     argv = sys.argv
+    exit = sys.exit
     if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -21,3 +22,5 @@ if __name__ == "__main__":
             print("{} {} {} = {}".format(a, b, c, calculator_1.sub(int(a), int(c))))
         if argv[2] == "*":
             print("{} {} {} = {}".format(a, b, c, calculator_1.mul(int(a), int(c))))
+        if argv[2] == "/":
+            print("{} {} {} = {}".format(a, b, c, calculator_1.div(int(a), int(c))))
