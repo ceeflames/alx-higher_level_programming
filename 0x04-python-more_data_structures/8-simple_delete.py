5cb5a43c6e4c29cacc62e2654a996059040fedb3
ceeflames <ceedizprodigy@gmail.com>
2023-08-16 23:48:21 -0700

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if value in a_dictionary:
-        del a_dictionary["{:s}".format(value)]
+    if key in a_dictionary:
+        del a_dictionary["{:s}".format(key)]
     return (a_dictionary)
