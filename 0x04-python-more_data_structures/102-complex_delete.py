39589110f50b902e106df857f865768d5a7797f1
ceeflames <ceedizprodigy@gmail.com>
2023-08-17 02:25:42 -0700

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    del_key = []
+
+    for key, values in a-dictionary.items():
+        if values == value:
+            del_key.append9key)
+
+    for key in del_key:
+        a_dictionary.pop(key, None)
