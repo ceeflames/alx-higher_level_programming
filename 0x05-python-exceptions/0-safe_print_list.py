59dacc106b8a921e75ef00c5c5ff0fe2278129dd
ceeflames <ceedizprodigy@gmail.com>
2023-08-28 10:49:10 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if my_list is None:
+        return (0)
+    count = 0
     try:
-        count = 0
 
         for elements in my_list:
             if count < x:
