37d617e32b117b69d66edd7816e357044ff4f1a4
ceeflames <ceedizprodigy@gmail.com>
2023-08-30 08:31:40 -0700

    2-square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Define a square
+"""
+
+
+class Square:
+    """A class square"""
+
+
+    def __init__(self, size=0):
+        """square with size constructor"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
