08f4843fa3c5f41a253179828677270b46131a98
ceeflames <ceedizprodigy@gmail.com>
2023-08-30 08:08:16 -0700

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Define class Square with size
+"""
+
+
+class Square:
+    """ The size in the square """
+
+    def __init__(self, square_size):
+        """the square constructor"""
+        self.__size = square_size
