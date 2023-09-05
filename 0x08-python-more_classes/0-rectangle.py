0fb47576671b5b566b6ec80530f4d3d9ba2ff82d
ceeflames <ceedizprodigy@gmail.com>
2023-09-05 07:27:57 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -7,12 +7,5 @@ class Rectangle:
     """
     rectangle class 
     """
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
+    def __init__(self):
+        pass
