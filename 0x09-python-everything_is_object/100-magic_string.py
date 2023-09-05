1cc8ee97d8ef48cc3d2603766e908412affa3032
ceeflames <ceedizprodigy@gmail.com>
2023-09-05 05:06:48 -0700

    answer

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string():
+    setattr(magic_string, "n", getattr(magoc_string, "n", 0) + 1)
+    return ("BestChool, " * getattr(magic_string, "n", 0))[:-2]
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    101-locked_class: class LockedClass
+"""
+
+class LockedClass:
+    """
+    A class with one attr first_name
+    first_name (str): name
+    """
+
+    __slots__ = ['first_name']
