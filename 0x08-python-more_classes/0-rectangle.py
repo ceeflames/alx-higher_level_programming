5f6335ddcec2009724fafb5c5a3af314e5053dea
ceeflames <ceedizprodigy@gmail.com>
2023-09-05 07:21:26 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
+"""
+Module composed by a function that defines a rectangle
+"""
 class Rectangle:
     """
-    Empty class
+    rectangle class 
     """
-    pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
