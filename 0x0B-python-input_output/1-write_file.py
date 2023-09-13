a7daa37cbd57f139d6b83ebf5b0ba9ba905ca5e9
ceeflames <ceedizprodigy@gmail.com>
2023-09-13 15:45:55 -0700

    write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,4 @@
 def write_file(filename="", text=""):
     """write text in filename with utf-8"""
     with open(filename, 'w', ebcoding="utf-8") as f:
-        writt_en = f.write(text)
-
-    return writt_en
+        return f.write(text)
