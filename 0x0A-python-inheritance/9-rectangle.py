0621c2bdb2677b7e56997ba9fd51c96b1c631503
ceeflames <ceedizprodigy@gmail.com>
2023-09-13 01:40:54 -0700

    9-geometry

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A rectangle class """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A subclass of the parent clasee BaseGeometry"""
+    def __init__(self, width, height):
+        """initialize"""
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """Area of the rectangle"""
+        area = self.__width * self.__height
+        return area
+
+    def __str__(self):
+        """magic method to return a string"""
+        return ("[{}] {}/{}".format(type(self).__name__, 
+            self.__width, self.__height))
