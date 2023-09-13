2aea9e25f0750ecedbdccd0f0c93b280cebdd4a6
ceeflames <ceedizprodigy@gmail.com>
2023-09-13 16:09:21 -0700

    json_string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that returns an object"""
+
+
+def from_json_string(my_str):
+    """Returns a python data structure represented by a JSON"""
+    return json.loads(my_str)
