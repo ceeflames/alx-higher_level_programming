a40a307c7be54639fe0f92e0107fdb544f44a62e
ceeflames <ceedizprodigy@gmail.com>
2023-09-12 22:29:17 -0700

    2-is_same

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines the is_same_class methog """
+
+def is_same_class(obj, a_class):
+    """ attributes of the function """
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
