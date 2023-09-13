3f6f30599f1f0a47f5539cdb74786156e1e8516e
ceeflames <ceedizprodigy@gmail.com>
2023-09-12 23:19:10 -0700

    inherits from py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Class basegeometry """
+
+
+class BaseGeometry:
+    """ Methods to inherit below """
+
+    def area(self):
+        raise Exception("area() is not implemented")
