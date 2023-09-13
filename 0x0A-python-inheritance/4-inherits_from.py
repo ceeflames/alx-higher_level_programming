6de189df515fd94ef43b4fd11e4e4e742532811b
ceeflames <ceedizprodigy@gmail.com>
2023-09-12 23:12:36 -0700

    inherits from py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-""" 
-Function on the obj instance of a class 
 """
-
+Function on the obj instance of a class
+"""
 
 
 def inherits_from(obj, a_class):
     """
-        The function returns True of the obj is an  instance of a class 
+        The function returns True of the obj is an  instance of a class
         that inherited from the specified class. otherwise False
     """
     if not type(obj) is a_class and issubclass(type(obj), a_class):
