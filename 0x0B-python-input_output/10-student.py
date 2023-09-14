b3f9937fad9b754c882ac8150e5b5437f9850d18
ceeflames <ceedizprodigy@gmail.com>
2023-09-13 18:25:51 -0700

    10-student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Dictionary reprentation of the student"""
+
+        if (type(attrs) == list and all(type(element) == str for element in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
