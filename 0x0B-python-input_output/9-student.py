7d7f156862f78740bf2bcab344124a06cf662f5e
ceeflames <ceedizprodigy@gmail.com>
2023-09-13 17:58:50 -0700

    student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A student class"""
+
+
+class Student:
+    """student"""
+    def __init__(self, first_name, last_name, age):
+        """initializer"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Student instance in a dictionatry"""
+        return sefl.__dict__
