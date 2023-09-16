330f0735089104dc3173b3a5390f51868ebd4e61
ceeflames <ceedizprodigy@gmail.com>
2023-09-16 06:42:58 -0700

    dict rep in square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,3 +39,12 @@ class Square(Rectangle):
         """magic method returns strings"""
         return ("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width))
+
+    def to_dictionary(self):
+        """Dictionary representation of the square"""
+        return {
+                "id" : self.id,
+                "size" : self.size,
+                "x" : self.x,
+                "y" : self.y
+                }
