7c45b6e3c7aa694b5c91f2a55c8cbd5f4972335a
ceeflames <ceedizprodigy@gmail.com>
2023-09-16 06:51:21 -0700

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """First class base"""
 
 
+import json
+
+
 class Base:
     """ private class attribute """
     __nb_objects = 0
@@ -14,3 +17,11 @@ class Base:
             """ increment __nb_object and assign the new value to id """
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON str representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
