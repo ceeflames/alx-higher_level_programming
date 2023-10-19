f08cd1058dc67f4bcd7874e4b4ce19cf37990d47
ceeflames <ceedizprodigy@gmail.com>
2023-10-19 09:30:40 -0700

    force_name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Script that creates the table force_name
-CREATE TABLE IF NOT EXISTS force_name (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS 'force_name' (
+	'id' INT AUTO_INCREMENT PRIMARY KEY,
+	'name' VARCHAR(256) NOT NULL
 	);
