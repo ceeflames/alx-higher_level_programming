4ef47e68ebed2574b0b59e20aa7b13e401686c18
ceeflames <ceedizprodigy@gmail.com>
2023-10-19 10:52:32 -0700

    first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
--- A table first_table in database
-CREATE TABLE 
-	IF NOT EXISTS 'first_table' (
-		'id' INT, 
-		'name' VARCHAR(256)
+-- Script that creates table first_table in database
+CREATE TABLE
+	IF NOT EXISTS first_table(
+		`id` INT, 
+		`name` VARCHAR(256)
 	);
