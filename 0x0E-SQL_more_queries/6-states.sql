b4798d41d20be04a347a28f310c4abeeacb3cb46
ceeflames <ceedizprodigy@gmail.com>
2023-10-19 10:07:21 -0700

    states_sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Script that creates the table hbtn_0d_usa
+-- creates table states in the database hbtn_0d_usa
+CREATE DATABASE
+	IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE
+	IF NOT EXISTS `hbtn_0d_usa`.`states` (
+		PRIMARY KEY(`id`),
+		`id` INT UNIQUE NOT NULL AUTO_INCREMENT,
+		`name` VARCHAR(256) NOT NULL
+		);
