9a326b2359e4470363d09abeea2dafc422f0dccf
ceeflames <ceeflames@yahoo.com>
2023-11-16 13:07:54 -0800

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (width, height) {
-    if (typeof wifth === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
       this.width = width;
       this.height = height;
     }
