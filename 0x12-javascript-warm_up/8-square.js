f797514927ab0cb6cf4a27cd32a55207b449ba57
ceeflames <ceeflames@yahoo.com>
2023-11-16 03:33:41 -0800

    Squares

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,8 +3,8 @@
 const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
-}	else	{
-  for (let i = 0; i < size; i++)	{
+} else {
+  for (let i = 0; i < size; i++) {
     let row = '';
     for (let j = 0; j < size; j++) row += 'X';
     console.log(row);
