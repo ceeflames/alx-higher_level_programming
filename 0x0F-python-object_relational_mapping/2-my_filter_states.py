e39bd8d977dde8a3b9b05becd457dab7be1a732d
ceeflames <ceedizprodigy@gmail.com>
2023-11-20 02:29:52 -0800

    2 filter states2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
             db=argv[3]
             )
 
-    cur = db.connect()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states \
             WHERE name LIKE BINARY '{}' \
             ORDER BY states.id ASC".format(argv[4]))
