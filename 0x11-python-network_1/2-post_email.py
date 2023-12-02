c8694da82d39cd6439a356ee477201c77f7c3f8c
ceeflames <ceeflames@yahoo.com>
2023-12-01 17:47:47 -0800

    POST an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL and an email
+    sends a POST request to the passed URL with the email as parameter
+    displays the body of the response(decoded in utf-8)
+"""
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email':email}).encode('ascii')
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
